c53d7f7b2bd3079213e2cd771ac0045f6f168373
Trinath Adusumilli <trinathadusumilli@gmail.com>
2022-10-26 15:11:14 -0500

    Added ssl configuration for prod profile

---
+++ src/main/java/com/twitter/mudbl/configuration/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.twitter.mudbl.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Profile("prod")
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .requiresChannel(channel ->
+                        channel.anyRequest().requiresSecure())
+                .authorizeRequests(authorize ->
+                        authorize.anyRequest().permitAll())
+                .build();
+    }
+}
